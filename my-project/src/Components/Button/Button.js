248f843246f08526cac6dba221c2644d31d6fcb4
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-03-08 13:37:29 +0330

    button style

--- my-project/src/Components/Button/Button.js
+++ my-project/src/Components/Button/Button.js
@@ -2,9 +2,9 @@ import El from "../../library/El";
 
 const variants = {
   contained:
-    "flex justify-center bg-red-700 text-white-me p-3  rounded-md w-28 hover:bg-red-600 hover:font-semibold h-10 items-center inline-block rounded-md text-sm py-2 px-6",
+    "flex justify-center bg-blue-700 text-white-me p-3  rounded-md w-24 hover:bg-blue-600 hover:font-semibold h-10 items-center inline-block rounded-md text-sm py-2 px-6",
   outlined:
-    "flex text-center inline-block border border-1 text-gray-500 font-thin border-gray-300 rounded-full text-lg h-9 items-center text-sm py-2 px-4",
+    "flex justify-center inline-block border border-1 text-blue-700 w-24 rounded-md  border-blue-700  text-lg h-9 items-center text-sm py-2 px-4",
 
   link: "flex bg-transparent p-3 hover:bg-gray-100 rounded-md w-20 justify-center text-gray-500 text-xs hover:text-black hover:font-semibold h-10 items-center inline-block rounded-md text-sm py-2 px-6",
   link2:
